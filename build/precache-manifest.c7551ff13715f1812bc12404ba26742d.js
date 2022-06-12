635480ab3a9783b84e27eab73250fd5e8325b3c6
dato <dato.tchkadua.1@iliauni.edu.ge>
2022-06-12 23:15:49 +0400

    Configure React app for deployment to GitHub Pages

---
+++ build/precache-manifest.c7551ff13715f1812bc12404ba26742d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b8956096eec91050224d34d3be83e4f",
+    "url": "/menu/index.html"
+  },
+  {
+    "revision": "1e3bb206869a0bd384d9",
+    "url": "/menu/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/menu/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/menu/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e3bb206869a0bd384d9",
+    "url": "/menu/static/js/main.5a703c2e.chunk.js"
+  },
+  {
+    "revision": "56ef0208991cca3c548d",
+    "url": "/menu/static/js/runtime-main.a5a74f46.js"
+  }
+]);
